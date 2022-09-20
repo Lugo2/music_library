ea67878d0872ca34920a6a19de3c54287818aaeb
Mario Lugo <mlugo23699@gmail.com>
2022-09-19 22:09:12 -0700

    modified project urls file endpoints + finished views and urls files' logic for music app

--- music_library_project/music/urls.py
+++ music_library_project/music/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.music_list),
+    path('<int:pk>/', views.music_library), 
+]
--- music_library_project/music/views.py
+++ music_library_project/music/views.py
@@ -5,3 +5,40 @@ from rest_framework import status
 from .serializers import MusicLibrarySerializer
 from .models import MusicLibrary
 
+@api_view(['GET', 'POST'])
+def music_list(request):
+    
+    if request.method == 'GET':
+        music_type = request.query_params.get('type')
+        queryset = MusicLibrary.objects.all()
+        if music_type:
+            queryset = queryset.filter(music_type__name = music_type)
+
+        serializer = MusicLibrarySerializer(queryset, many = True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MusicLibrarySerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def music_library(request, pk):
+
+    music_library = get_object_or_404(MusicLibrary, pk = pk)
+
+    if request.method == 'GET':
+        serializer = MusicLibrarySerializer(music_library)
+        return Response(serializer.id)
+
+    elif request.method == 'PUT':
+        serializer = MusicLibrarySerializer(music_library, data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        music_library.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
