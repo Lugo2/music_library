7f6b54ac7967aa13fa4b8302d3bc1a5a46686f3f
Mario Lugo <mlugo23699@gmail.com>
2022-09-19 22:05:11 -0700

    generated a serializer file and created serializer class for music app + made migrations

--- music_library_project/music/serializers.py
+++ music_library_project/music/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import MusicLibrary
+
+class MusicLibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MusicLibrary
+        fields = ['id', 'tile','artist','album','release_date','genre']
+        depth = 1
