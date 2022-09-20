885dd96f90fa53b3317d755b60acadc61dfdbd61
Mario Lugo <mlugo23699@gmail.com>
2022-09-19 22:01:50 -0700

    created models for music app + added music app to settings INSTALLED_APPS

--- music_library_project/music/models.py
+++ music_library_project/music/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class MusicLibrary(models.Model):
+    tile = models.CharField(max_length = 250)
+    artist = models.CharField(max_length = 250)
+    album = models.CharField(max_length = 400)
+    release = models.DateField(max_length = 400)
+    genre = models.CharField(max_length = 400)
